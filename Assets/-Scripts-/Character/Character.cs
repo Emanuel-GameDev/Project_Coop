082a72d3f8b5e17d119f919da7583d9b7897035b
xGiordan23x <francigio23@gmail.com>
2023-10-03 16:26:45 +0200

    prova Script

---
+++ Assets/-Scripts-/Character/Character.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private float maxHp;
+    [SerializeField] private float currentHp;
+    [SerializeField] private float speed;
+    
+
+    
+    public virtual void Move()
+    {
+
+    }
+    public virtual void Attack()
+    {
+
+    } 
+    public virtual void Defend()
+    { 
+    }
+
+}
