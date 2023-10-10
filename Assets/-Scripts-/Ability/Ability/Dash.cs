4caa54b0e6ac45c19ff50f1473f6c870d968313f
Kyldros <kyldros@gmail.com>
2023-10-10 16:10:46 +0200

    MoveUpdadte

--- Assets/-Scripts-/Ability/Ability/Dash.cs
+++ Assets/-Scripts-/Ability/Ability/Dash.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dash : MonoBehaviour
+[CreateAssetMenu(menuName = "Ability/Defence/Dash")]
+public class Dash : Defence
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
