7e0232430a0ebe9087d4c5a623e6bb5710d9182f
AndreaSforzi <125589887+AndreaSforzi@users.noreply.github.com>
2023-10-24 17:42:35 +0200

    Update Healer.cs

--- Assets/-Scripts-/Character/Players/Healer.cs
+++ Assets/-Scripts-/Character/Players/Healer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Healer : MonoBehaviour
+public class Healer : CharacterClass
 {
     // Start is called before the first frame update
     void Start()
