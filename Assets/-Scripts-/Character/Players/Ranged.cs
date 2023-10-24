6ac9aad6ed64d9dec8d4952cb81d34af9275c538
Fede1900 <federome@live.it>
2023-10-24 17:50:12 +0200

    riiniziato sniper

--- Assets/-Scripts-/Character/Players/Ranged.cs
+++ Assets/-Scripts-/Character/Players/Ranged.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Ranged : CharacterClass
 {
-    
+    public override void Attack(Character parent)
+    {
+        base.Attack(parent);
+    }
+
+    public override void Defence(Character parent)
+    {
+        base.Defence(parent);
+    }
+
+    public override void UseExtraAbility(Character parent)
+    {
+        base.UseExtraAbility(parent);
+    }
+
+    public override void UseUniqueAbility(Character parent)
+    {
+        base.UseUniqueAbility(parent);
+    }
 }
