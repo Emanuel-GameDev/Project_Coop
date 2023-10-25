6b0b90d6edc7de8e62ea3a5a3e6ea5afecb0db24
Kyldros <kyldros@gmail.com>
2023-10-25 10:34:51 +0200

    Update Utility.cs

--- Assets/-Scripts-/Generics/Utility.cs
+++ Assets/-Scripts-/Generics/Utility.cs
@@ -1,12 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public static class Utility
 {
-    public static bool IsInLayerMask(this LayerMask targetMask, int layer)
+
+    /// <summary>
+    /// Controlla se un oggetto è nella layer mask specificata.
+    /// </summary>
+    /// <param name="obj">L'oggetto da controllare.</param>
+    /// <param name="layerMask">La layer mask da verificare.</param>
+    /// <returns>Restituisce true se l'oggetto è nella layer mask, altrimenti restituisce false.</returns>
+    public static bool IsInLayerMask(GameObject obj, LayerMask layerMask)
+    {
+        int objLayerMask = 1 << obj.layer;
+        return (layerMask.value & objLayerMask) > 0;
+    }
+
+    /// <summary>
+    /// Verifica se il layer di un oggetto è presente in una LayerMask.
+    /// </summary>
+    /// <param name="targetMask">La LayerMask da controllare.</param>
+    /// <param name="gameObject">L'oggetto il cui layer deve essere verificato.</param>
+    /// <returns>Restituisce true se il layer dell'oggetto è presente nella LayerMask, altrimenti restituisce false.</returns>
+    public static bool IsInLayerMask(int layer, LayerMask targetMask)
     {
         int layerbit = 1 << layer;
         return (targetMask & layerbit) != 0;
     }
+
 }
