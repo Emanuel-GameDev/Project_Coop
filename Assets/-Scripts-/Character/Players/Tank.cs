afcc36b766edcfa994e546f45dc652555461ca6c
xGiordan23x <francigio23@gmail.com>
2023-11-07 17:18:06 +0100

    animator risolto e robe tank

--- Assets/-Scripts-/Character/Players/Tank.cs
+++ Assets/-Scripts-/Character/Players/Tank.cs
@@ -1,22 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Tank : CharacterClass
 {
+    [Header("Block")]
+    [SerializeField, Tooltip("Quantità di danno parabile prima di rottura parata")]
+    float staminaBlock;
+    [SerializeField, Tooltip("Danno parata perfetta (Potenziamento 4)")]
+    float perfectBlockDamage;
+
+    [Header("Unique Ability")]
+    [SerializeField, Tooltip("Durata aggro")]
+    float aggroDuration;
+    [SerializeField, Tooltip("Durata buff difesa")]
+    float defenceBuffDuration;
+    [SerializeField, Tooltip("Moltiplicatore buff difesa")]
+    float defenceMultyplier;
+
+    [Header("Bossfight Upgrade")]
+    [SerializeField, Tooltip("Numero attacchi da parare perfettamente per ottenimento potenziamento bossfight")]
+    int attacksToBlockForUpgrade = 10;
+    [SerializeField, Tooltip("Cooldown attacco potenziamento bossfight")]
+    float cooldownExtraAbility;
+    [SerializeField, Tooltip("Durata stun attacco potenziamento boss fight")]
+    float chargedAttackStunDuration;
+    [SerializeField, Tooltip("Moltiplicatore durata stun (Potenziamento 2)")]
+    float stunDurationMultyplier;
+
+
+    private bool canDoubleAttack => upgradeStatus[AbilityUpgrade.Ability1];
+    private bool hyperArmorUnlocked => upgradeStatus[AbilityUpgrade.Ability3];
+    private bool hyperArmorOn;
+    private bool isAttacking = false;
+    private int comboIndex = 0;
+    private int comboMax = 2;
+
+
+    //se potenziamento 1 ha 2 attacchi
     public override void Attack(Character parent)
     {
-        base.Attack(parent);
-        //se potenziamento 1 ha 2 attacchi
-        //se potenziamento 3 attiva hyperArmor
+        isAttacking = true;  
+        if(comboIndex == 0)
+        {
+            animator.SetTrigger("Attack1");
+        }
+        //se potenziamento 3 attiva hyperArmor da animazione
 
         //se potenziamento boss fight attacco caricato 
         //se potenziamento 5 attacco caricato (Da decidere)
+        Debug.Log($"Attack[{comboIndex}  canDoubleAttack[{canDoubleAttack}  hasHyperArmor[{hyperArmorUnlocked}]");
     }
     public override void Defence(Character parent)
     {
         base.Defence(parent);
-        //se potenziamento 4 ha parata perfetta
+        //se potenziamento 4 parata perfetta fa danno
     }
     public override void UseExtraAbility(Character parent)
     {
@@ -30,6 +67,28 @@ public class Tank : CharacterClass
     }
     public override void TakeDamage(float damage, Damager dealer)
     {
+        if(hyperArmorOn == false)
+        {
+            DoHitReacion();
+        }
+        
         //se sta attacando e potenziamento 3 sbloccato non interrompe attacco
+    }
+
+    private void DoHitReacion()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ActivateHyperArmor()
+    {
+        if(hyperArmorUnlocked)
+        hyperArmorOn = true;
+
+        Debug.Log("hyper armor on");
+    }
+    public void DeactivateHyperArmor()
+    { 
+        hyperArmorOn = false;
     }
 }
