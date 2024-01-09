52b005201394155a4f6ee2c3422def089362ba0a
AndreaSforzi <125589887+AndreaSforzi@users.noreply.github.com>
2024-01-09 15:04:55 +0100

    corretto errore di merge

--- Assets/-Scripts-/Generics/StateMachine/TutorialStates/IntermediateTutorialFase.cs
+++ Assets/-Scripts-/Generics/StateMachine/TutorialStates/IntermediateTutorialFase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntermediateTutorialFase : State
+public class IntermediateTutorialFase : TutorialFase
 {
     TutorialManager tutorialManager;
 
