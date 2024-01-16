762fee91abaf89d32a1dbf90971b321068b80a8f
AndreaSforzi <andreasforzi01@gmail.com>
2024-01-16 15:04:24 +0100

    Basic enemy

--- Assets/-Scripts-/Character/Enemies/SimpleEnemy/BasicEnemy.cs
+++ Assets/-Scripts-/Character/Enemies/SimpleEnemy/BasicEnemy.cs
@@ -9,29 +9,30 @@ public class BasicEnemy : EnemyCharacter
     [SerializeField] float stoppingDistance = 1;
 
 
-    [SerializeField] private ContextSolver moveDirectionSolver;
 
     //di prova
     [SerializeField] Transform tryTarget;
 
-    [SerializeField] NavMeshAgent agent1;
     NavMeshPath path;
 
+    [SerializeField] Transform pivot;
+    Vector2 lastNonZeroDirection;
+    bool isMoving;
+    bool isAttacking = false;
 
     private void Start()
     {
         path = new NavMeshPath();
     }
+
     private void Update()
     {
-        if (agent1.CalculatePath(tryTarget.position, path))
+        if (agent.CalculatePath(tryTarget.position, path))
         {
-            for(int i = 0; i < path.corners.Length;i++)
-            {
-                Debug.Log(path.corners[i]);
-            }
-            Move(path.corners[1] - path.corners[0], rb);
-
+            if (path.corners.Length > 1)
+                Move(path.corners[1] - path.corners[0], rb);
+            else
+                Move(tryTarget.position - transform.position, rb);
         }
         else
             rb.velocity = Vector3.zero;
@@ -39,58 +40,68 @@ public class BasicEnemy : EnemyCharacter
 
     public virtual void Move(Vector3 direction, Rigidbody rb)
     {
-        
-
-        if (direction.magnitude < stoppingDistance)
+        if (Vector3.Distance(transform.position,tryTarget.position) < stoppingDistance)
         {
             rb.velocity = Vector3.zero;
+
+            //attacca
+            if(!isAttacking)
+                StartCoroutine(Attack());
+
             return;
         }
-
+        
         if (!direction.normalized.Equals(direction))
             direction = direction.normalized;
 
         rb.velocity = new Vector3(direction.x * MoveSpeed, direction.y, direction.z * MoveSpeed);
 
+
+
+
+        Vector2 direction2D = new Vector2(direction.x, direction.z);
+
+        isMoving = rb.velocity.magnitude > 0.2f;
         
 
+        if (direction2D != Vector2.zero)
+            lastNonZeroDirection = direction2D;
 
+        SetSpriteDirection(lastNonZeroDirection);
 
-
-        //forse non serve
-        //Vector2 direction2D = new Vector2(direction.x, direction.z);
-
-        //isMoving = rb.velocity.magnitude > 0.2f;
-
-        //if (direction2D != Vector2.zero)
-        //    lastNonZeroDirection = direction2D;
-        //SetSpriteDirection(lastNonZeroDirection);
+        animator.SetBool("isMooving", isMoving);
     }
 
-    Vector3 FindDirection()
+    protected void SetSpriteDirection(Vector2 direction)
     {
-        //trova direzione con context solver
+        if (direction.y != 0)
+            animator.SetFloat("Y", direction.y);
 
-        Vector3 moveVector = tryTarget.position - transform.position;
-        return moveVector;
+        Vector3 scale = pivot.gameObject.transform.localScale;
+
+        if ((direction.x > 0.5 && scale.x > 0) || (direction.x < -0.5 && scale.x < 0))
+            scale.x *= -1;
+
+        pivot.gameObject.transform.localScale = scale;
     }
+
+    IEnumerator Attack()
+    {
+        isAttacking = true;
+        GetComponentInChildren<SpriteRenderer>().material.color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        isAttacking = false;
+        GetComponentInChildren<SpriteRenderer>().material.color = Color.white;
+    }
+
 
     public override void TargetSelection()
     {
         //base.TargetSelection();
-        
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, stoppingDistance);
-        if(path != null)
-        {
-            if (path.corners.Length > 0)
-            {
-                Gizmos.DrawLineList(path.corners);
-            }
-
-        }
     }
 }
