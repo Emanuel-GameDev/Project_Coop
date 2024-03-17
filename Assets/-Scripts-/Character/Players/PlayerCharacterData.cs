cad6f64d2088a09fdbef837c76866d01a80c7d3f
Kyldros <kyldros@gmail.com>
2024-03-17 14:56:46 +0100

    mlml

--- Assets/-Scripts-/Character/Players/PlayerCharacterData.cs
+++ Assets/-Scripts-/Character/Players/PlayerCharacterData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 [CreateAssetMenu(fileName = "PlayerCharacterData", menuName = "Character/PlayerCharacterData")]
 public class PlayerCharacterData : ScriptableObject
@@ -19,7 +20,7 @@ public class PlayerCharacterData : ScriptableObject
     [SerializeField]
     private Sprite dialogueSprite;
     [SerializeField]
-    private Sprite pixelSprite;
+    private SpriteLibraryAsset pixelAnimations;
     [SerializeField]
     private Sprite pixelFaceSprite;
 
@@ -29,7 +30,7 @@ public class PlayerCharacterData : ScriptableObject
     public Sprite FullBodyArt => fullBodyArt;
     public Sprite HudSprite => hudSprite;
     public Sprite DialogueSprite => dialogueSprite;
-    public Sprite PixelSprite => pixelSprite;
+    public SpriteLibraryAsset PixelAnimations => pixelAnimations;
     public Sprite PixelFaceSprite => pixelFaceSprite;
 
 }
