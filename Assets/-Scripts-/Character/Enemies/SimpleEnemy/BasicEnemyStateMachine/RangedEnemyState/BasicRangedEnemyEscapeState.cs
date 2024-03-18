4f10f6337b043ba6f213aa88be5e569c7ec94c3b
AndreaSforzi <andreasforzi01@gmail.com>
2024-03-18 19:45:32 +0100

    build fix 1

--- Assets/-Scripts-/Character/Enemies/SimpleEnemy/BasicEnemyStateMachine/RangedEnemyState/BasicRangedEnemyEscapeState.cs
+++ Assets/-Scripts-/Character/Enemies/SimpleEnemy/BasicEnemyStateMachine/RangedEnemyState/BasicRangedEnemyEscapeState.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEditor.PlayerSettings;
 
 public class BasicRangedEnemyEscapeState : BasicEnemyState
 {
