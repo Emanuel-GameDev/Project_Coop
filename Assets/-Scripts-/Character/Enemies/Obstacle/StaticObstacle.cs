e2cb2f12a11c9fa8919de3bbccf8e1ff7ea713d8
Fede1900 <federome@live.it>
2024-03-22 18:31:56 +0100

    qualche ostacolo fatto
    riguardare quello essplosivo

--- Assets/-Scripts-/Character/Enemies/Obstacle/StaticObstacle.cs
+++ Assets/-Scripts-/Character/Enemies/Obstacle/StaticObstacle.cs
@@ -21,10 +21,7 @@ public class StaticObstacle : ObstacleEnemy
         if (collision.gameObject.TryGetComponent(out PlayerCharacter player))
         {
             Debug.Log("sono entrato nelle spine");
-            //Vector2 forceVector= (player.transform.position- transform.position).normalized;
-
             
-            //player.gameObject.GetComponentInChildren<Rigidbody2D>().AddForce(forceVector * pushStrength,ForceMode2D.Force);
 
             StartCoroutine(PushPlayer(player));
 
@@ -32,35 +29,6 @@ public class StaticObstacle : ObstacleEnemy
         }
     }
 
-    private IEnumerator PushPlayer(PlayerCharacter player)
-    {
-        float timer = 0;
-        float interpolationRatio;
-        Vector3 startPoint=player.transform.position;
-
-        Vector3 forceVector = (player.transform.position - transform.position).normalized;
-
-        while (timer<1)
-        {
-            if (player.gameObject.TryGetComponent(out Rigidbody2D rb))
-            {
-               
-                interpolationRatio = timer / 1;
-                timer += Time.deltaTime;
-                rb.MovePosition(Vector3.Lerp(startPoint, startPoint + (forceVector * pushStrength), AnimationCurve.Evaluate(interpolationRatio))); 
-                yield return new WaitForFixedUpdate();
-                
-            }
-        }
-        
-
-        yield return null;
-    }
-
-    void AddExplosionForce2D(PlayerCharacter player,Vector3 explosionOrigin, float explosionForce, float explosionRadius)
-    {
-        Vector2 direction = player.transform.position - explosionOrigin;
-        float forceFalloff = 1 - (direction.magnitude / explosionRadius);
-        player.gameObject.GetComponent<Rigidbody2D>().AddForce(direction.normalized * (forceFalloff <= 0 ? 0 : explosionForce) * forceFalloff);
-    }
+    
+    
 }
