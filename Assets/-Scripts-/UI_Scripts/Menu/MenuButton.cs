b3cdec26373a686bf82f416f11db9922a633c2a2
Kyldros <kyldros@gmail.com>
2024-03-26 17:51:40 +0100

    diooooooooo

--- Assets/-Scripts-/UI_Scripts/Menu/MenuButton.cs
+++ Assets/-Scripts-/UI_Scripts/Menu/MenuButton.cs
@@ -2,9 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class MenuButton : MonoBehaviour
+public class MenuButton : Button
 {
-    [SerializeField]
-    UnityEvent onButtonClicked = new UnityEvent();
+    GameObject highlight;
+
+    protected override void Start()
+    {
+        base.Start();
+        highlight = GetComponentInChildren<Highlight>().gameObject;
+    }
+
+    protected override void DoStateTransition(SelectionState state, bool instant)
+    {
+        base.DoStateTransition(state, instant);
+        if(state == SelectionState.Selected)
+        {
+            highlight.SetActive(true);
+        }
+        if(state == SelectionState.Normal)
+        {
+            highlight.SetActive(false);
+        }
+    }
+
+
 }
+
+  
+
+
