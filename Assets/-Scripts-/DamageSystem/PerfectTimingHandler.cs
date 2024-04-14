f8e7f6de2b9f07dd68b0887b99d3d198ff770a83
Kyldros <kyldros@gmail.com>
2024-04-14 10:23:21 +0200

    Inizio Revisione PerfectTime

--- Assets/-Scripts-/DamageSystem/PerfectTimingHandler.cs
+++ Assets/-Scripts-/DamageSystem/PerfectTimingHandler.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class PerfectTimingHandler : MonoBehaviour
 {
-    
+    [SerializeField] GameObject alertObject;
+    [SerializeField] IPerfectTimeReceiver receiver;
+
+    private void Awake()
+    {
+        receiver ??= GetComponentInParent<IPerfectTimeReceiver>();
+        if(receiver == null)
+            Debug.LogError("No receiver set on perfect timing handler");
+        else
+            receiver.SetPerfectTimingHandler(this);
+
+
+        DeactivateAlert();
+
+        if(alertObject == null)
+            Debug.LogError("No alert object set on perfect timing handler");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        receiver.PerfectTimeStarted();
+    }
+
+    public void ActivateAlert()
+    {
+        if (alertObject != null)
+            alertObject.SetActive(true);
+    }
+
+    public void DeactivateAlert()
+    {
+        if (alertObject != null)
+            alertObject.SetActive(false);
+    }
+
 }
