1f709da56772bf4b3a93702f9aff935957a87835
Kyldros <kyldros@gmail.com>
2024-04-20 10:06:27 +0200

    Dps no hit animation

--- Assets/-Scripts-/UI_Scripts/InteractionNotificationHandler.cs
+++ Assets/-Scripts-/UI_Scripts/InteractionNotificationHandler.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class InteractionNotificationHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] 
+    private GameObject notificationPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 }
