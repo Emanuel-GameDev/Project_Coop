efeefa739994d003fcfa5494fe071bc667ad5412
xGiordan23x <francigio23@gmail.com>
2024-05-05 15:25:47 +0200

    audioo

--- Assets/-Scripts-/Character/Players/Tank.cs
+++ Assets/-Scripts-/Character/Players/Tank.cs
@@ -742,26 +742,5 @@ public class Tank : PlayerCharacter, IPerfectTimeReceiver
     }
     #endregion
 
-    #region Audio
-
-    public void OnFirstAttackSound()
-    {
-        AudioManager.Instance.PlayAudioClip(soundsDatabase.attackSounds[0], transform);
-    }
-    public void OnSecondAttackSound()
-    {
-        AudioManager.Instance.PlayAudioClip(soundsDatabase.attackSounds[1], transform);
-    }
-    public void OnBlockSound()
-    {
-        AudioManager.Instance.PlayAudioClip(soundsDatabase.blockSounds[0], transform);
-    }
-    public void OnWalkSound()
-    {
-        AudioManager.Instance.PlayAudioClip(soundsDatabase.walkSounds[0], transform);
-    }
-
-    #endregion
-
-
+   
 }
