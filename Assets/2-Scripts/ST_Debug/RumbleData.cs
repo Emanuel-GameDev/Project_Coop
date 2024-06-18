10aa6ff7aacdd761358ed9c48fa65e54a33029ec
Kyldros <kyldros@gmail.com>
2024-06-18 14:10:17 +0200

    RumbleUpdate

--- Assets/2-Scripts/ST_Debug/RumbleData.cs
+++ Assets/2-Scripts/ST_Debug/RumbleData.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 [Serializable]
 public class RumbleData
 {
+    public string rumbleName = "";
+
     [Range(0f, 1f)]
     public float lowFreqency;
 
     [Range(0f, 1f)]
     public float highFreqency;
 
+    public bool hasDuration = true;
+
+    [Min(0f)]
     public float duration;
 
     public int priority = 0;
-
-    public string rumbleName = "";
 
 }
